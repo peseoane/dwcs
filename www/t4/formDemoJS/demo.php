21b0fa6d2f5cb1c14b40de4290ba6b5f2278deae
Pedro Seoane Prado <info@pedroseoaneprado.es>
2023-10-25 10:58:48 +0200

    sync form php

---
+++ www/t4/formDemoJS/demo.php
@@ -0,0 +1,29 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['file'])) {
+    $uploadedFiles = $_FILES['file'];
+    $varDumpResults = array();
+
+    foreach ($uploadedFiles['name'] as $key => $fileName) {
+        $varDumpResult = processUploadedFile($uploadedFiles, $key);
+        $varDumpResults[] = generateResultCard($varDumpResult);
+    }
+
+    echo implode('', $varDumpResults);
+} else {
+    echo generateResultCard("ERROR: No se ha proporcionado ningún archivo.");
+}
+
+function processUploadedFile($uploadedFiles, $key) {
+    if ($uploadedFiles['error'][$key] === UPLOAD_ERR_OK) {
+        ob_start();
+        var_dump($uploadedFiles);
+        return ob_get_clean();
+    } else {
+        return "ERROR: " . $uploadedFiles['error'][$key];
+    }
+}
+
+function generateResultCard($varDumpResult) {
+    return '<div class="card text-bold">' . $varDumpResult . '</div>';
+}
+?>
