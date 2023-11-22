9c20c8e77a3400400d670a0db6ce87012f777070
Pedro Seoane Prado <info@pedroseoaneprado.es>
2023-11-22 09:32:10 +0100

    Prácticas de cookies

---
+++ www/pruebasCookies/ejercicios/practica1.php
@@ -0,0 +1,69 @@
+<?php
+declare(strict_types=1);
+
+session_start();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $cookieName = $_POST['cookieName'] ?? null;
+    $cookieValue = $_POST['cookieValue'] ?? null;
+    $cookieLifespan = !empty($_POST['cookieLifespan']) ? $_POST['cookieLifespan'] : 30;
+
+    if (!empty($cookieName) && !empty($cookieValue) && !empty($cookieLifespan)) {
+        $expiration = time() + (int)$cookieLifespan;
+        setcookie($cookieName, $cookieValue, $expiration);
+        header("Location: practica1.php");
+        exit;
+    }
+
+    if (isset($_POST['deleteCookies'])) {
+        foreach ($_COOKIE as $name => $value) {
+            setcookie($name, '', time() - 3600);
+        }
+        header("Location: practica1.php");
+        exit;
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Cookie Manager</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../css/bootstrap.css">
+</head>
+<body class="container p-4">
+<h1>Cookie Manager</h1>
+
+<h2>Current Cookies</h2>
+<ul>
+    <?php
+    foreach ($_COOKIE as $name => $value) {
+        echo '<li>' . htmlspecialchars($name) . ': ' . htmlspecialchars($value) . '</li>';
+    }
+    ?>
+</ul>
+
+<h2>Add a New Cookie</h2>
+<form method="post" action="">
+    <label>
+        Cookie Name:
+        <input type="text" name="cookieName" required>
+    </label>
+    <label>
+        Cookie Value:
+        <input type="text" name="cookieValue" required>
+    </label>
+    <label>
+        Cookie Lifespan (in seconds):
+        <input type="number" name="cookieLifespan">
+    </label>
+    <input type="submit" value="Add Cookie">
+</form>
+
+<h2>Delete All Cookies</h2>
+<form method="post" action="">
+    <input type="submit" name="deleteCookies" value="Delete All Cookies">
+</form>
+</body>
+</html>
---
+++ www/pruebasCookies/ejercicios/practica2.php
@@ -0,0 +1,72 @@
+<?php
+declare(strict_types=1);
+session_start();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $textValue = $_POST['textValue'] ?? null;
+}
+
+if (isset($_POST['deleteSession'])) {
+    session_destroy();
+    header("Location: practica2.php");
+    exit;
+}
+
+if (!empty($textValue)) {
+    if (isset($_SESSION['textValue']) && is_array($_SESSION['textValue'])) {
+        $_SESSION['textValue'][] = $textValue;
+    } else {
+        $_SESSION['textValue'] = [$textValue];
+    }
+    header("Location: practica2.php");
+    exit;
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Cookie Manager</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../css/bootstrap.css">
+    <script>
+        window.onload = function () {
+            const link = document.querySelector('link[href="../../css/bootstrap.css"]');
+            const request = new XMLHttpRequest();
+            request.open('GET', link.href, true);
+            request.onreadystatechange = function () {
+                if (request.readyState === 4) {
+                    if (request.status === 404) {
+                        link.href = "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css";
+                    }
+                }
+            };
+            request.send();
+        }
+    </script>
+</head>
+<body class="container p-4">
+<h1>Cookie Manager</h1>
+
+<h2>Current Cookies</h2>
+<pre>
+    <?php
+    var_dump($_SESSION);
+    ?>
+</pre>
+
+<h2>Add new data to the session</h2>
+<form method="post" action="">
+    <label>
+        Text value:
+        <input type="text" name="textValue" required>
+    </label>
+    <input type="submit" value="Add text">
+</form>
+<h3>Delete session</h3>
+<form method="post" action="">
+    <input type="submit" name="deleteSession" value="Delete session">
+</form>
+</body>
+</html>
