f1604f1464bade9c97923cb23287d25891e55250
Pedro Seoane Prado <info@pedroseoaneprado.es>
2023-11-24 09:40:23 +0100

    PDO Practica 1

--- www/pdo/practica1.php
+++ www/pdo/practica1.php
@@ -1,11 +1,11 @@
 <?php
 declare(strict_types=1);
 
-const MYSQL_USER = 'dwcs';
-const MYSQL_ROOT_PASSWORD = 'dwcs';
+const MYSQL_USER = 'root';
+const MYSQL_ROOT_PASSWORD = 'root';
 const MYSQL_HOST = 'mysql8';
-const MYSQL_DB = 'dwcs';
-CONST MYSQL_DSN = 'mysql:host=' . MYSQL_HOST . ';dbname=' . MYSQL_DB;
+const MYSQL_DB = 'recetas';
+const MYSQL_DSN = 'mysql:host=' . MYSQL_HOST . ';dbname=' . MYSQL_DB;
 
 try {
     $pdo = new PDO(MYSQL_DSN, MYSQL_USER, MYSQL_ROOT_PASSWORD);
@@ -15,3 +15,131 @@ try {
     echo 'Connection failed: ' . $e->getMessage();
     exit;
 }
+
+$sql = "SELECT receta.*, chef.nombre AS chef_name FROM receta LEFT JOIN chef ON receta.cod_chef = chef.codigo";
+$stmt = $pdo->prepare($sql);
+$stmt->execute();
+$recetas = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$columnNames = array_keys($recetas[0]);
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Recetas</title>
+</head>
+
+<body>
+<h1>Recetas</h1>
+<table>
+    <tr>
+        <?php foreach ($columnNames as $columnName): ?>
+            <?php if ($columnName != 'cod_chef'): ?>
+                <th><?= $columnName ?></th>
+            <?php endif; ?>
+        <?php endforeach; ?>
+    </tr>
+    <?php foreach ($recetas as $receta): ?>
+        <tr>
+            <?php foreach ($receta as $columnName => $columnValue): ?>
+                <?php if ($columnName != 'cod_chef'): ?>
+                    <td><?= $columnValue ?></td>
+                <?php endif; ?>
+            <?php endforeach; ?>
+        </tr>
+    <?php endforeach; ?>
+</table>
+</body>
+<style>
+    body {
+        font-family: Verdana, sans-serif;
+        font-size: 14px;
+        text-align: justify;
+    }
+
+    div#contenido {
+        width: 700px;
+        margin-left: auto;
+        margin-right: auto;
+    }
+
+    div#filtro {
+        width: 660px;
+    }
+
+    fieldset {
+        margin: 0px 0px 0px 23px;
+    }
+
+    h1 {
+        text-align: left;
+        color: grey;
+        margin: 1em;
+    }
+
+    h2 {
+        padding-left: 1em;
+        color: rosybrown;
+    }
+
+    table {
+        margin: 0px 10px 10px;
+        background-color: LightGrey;
+        border-collapse: collapse;
+        border-bottom: maroon 3px solid;
+    }
+
+    tr {
+        border-top: maroon 3px solid;
+    }
+
+    td, th {
+        padding: 10px 15px;
+        color: maroon;
+        text-align: left;
+    }
+
+    td a {
+        color: SlateGrey;
+        text-decoration: none;
+    }
+
+    #paginado {
+        text-align: left;
+        padding-left: 20em;
+    }
+
+    .ingredientes {
+        padding-left: 3em;
+        color: slategrey
+    }
+
+    table.edicion {
+        border-bottom: 0px;
+    }
+
+    table.edicion tr {
+        border-top: 0px;
+    }
+
+    table.edicion td {
+        padding: 5px 10px;
+    }
+
+    button {
+        margin: 5px;
+        padding: 5px 15px;
+    }
+
+    input[type="text"] {
+        width: 170px;
+    }
+
+    p.mensaje {
+        color: blue;
+        font-style: italic;
+    }
+</style>
+</html>
